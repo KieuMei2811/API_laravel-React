a928b37937cf65c155c1e5bdf405ab06b98cd123
KieuMei2811 <hothikieu28112003@gmail.com>
2023-06-29 01:22:03 +0700

    Deploy

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -160,12 +160,7 @@ class ShowProduct extends Component {
             name: "Image",
             sortable: true,
             cell: (row) => (
-                <img
-                    data-tag="allowRowEvents"
-                    src={`http://localhost:8000/source/image/product/${row.image}`}
-                    alt="preview"
-                    style={{ width: "100px" }}
-                />
+                <img data-tag="allowRowEvents"  src={`http://localhost:8000/source/image/product/${row.image}`} alt="preview"style={{ width: "100px" }} />
             ),
         },
         {
@@ -249,92 +244,37 @@ class ShowProduct extends Component {
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="inputPrice">Price</label>
-                                            <input
-                                                type="number"
-                                                min={10000}
-                                                className="form-control"
-                                                name="inputPrice"
-                                                id="inputPrice"
-                                                placeholder="Enter price"
-                                                required
-                                            />
+                                            <input type="number" min={10000} className="form-control" name="inputPrice" id="inputPrice" placeholder="Enter price" required />
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="inputPromotionPrice">
                                                 Promotion Price
                                             </label>
-                                            <input
-                                                type="number"
-                                                min={10000}
-                                                className="form-control"
-                                                name="inputPromotionPrice"
-                                                id="inputPromotionPrice"
-                                                placeholder="Enter promotion price"
-                                            />
+                                            <input type="number" min={10000} className="form-control" name="inputPromotionPrice" id="inputPromotionPrice" placeholder="Enter promotion price" />
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="inputUnit">Unit</label>
-                                            <input
-                                                type="text"
-                                                className="form-control"
-                                                name="inputUnit"
-                                                id="inputUnit"
-                                                placeholder="Enter unit"
-                                                required
-                                            />
+                                            <input type="text" className="form-control" name="inputUnit" id="inputUnit"  placeholder="Enter unit" required />
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="inputNew">New</label>
-                                            <input
-                                                type="number"
-                                                min={0}
-                                                className="form-control"
-                                                name="inputNew"
-                                                id="inputNew"
-                                                placeholder="Enter new"
-                                                required
-                                            />
+                                            <input type="number" min={0} className="form-control"name="inputNew" id="inputNew" placeholder="Enter new" required />
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="inputType">Type</label>
-                                            <input
-                                                type="number"
-                                                min={1}
-                                                className="form-control"
-                                                name="inputType"
-                                                id="inputType"
-                                                placeholder="Enter type"
-                                                required
-                                            />
+                                            <input type="number" min={1} className="form-control" name="inputType" id="inputType" placeholder="Enter type" required/>
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="inputImage">Image file</label>
-                                            <input
-                                                type="file"
-                                                className="form-control-file"
-                                                name="inputImage"
-                                                id="inputImage"
-                                                onChange={(e) => this.handleChange(e.target.files)}
-                                                required
-                                            />
+                                            <input type="file"className="form-control-file" name="inputImage" id="inputImage" onChange={(e) => this.handleChange(e.target.files)}required />
                                         </div>
                                         <div className="form-group">
-                                            <img
-                                                id="preview-image-before-upload"
-                                                src="https://www.riobeauty.co.uk/images/product_image_not_found.gif"
-                                                alt="xem trước"
-                                                style={{ maxHeight: 250 }}
-                                            />
+                                            <img id="preview-image-before-upload"src="https://www.riobeauty.co.uk/images/product_image_not_found.gif" alt="xem trước" style={{ maxHeight: 250 }}/>
                                             {this.previewImage()}
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="inputDescription">Description</label>
-                                            <input
-                                                type="text"
-                                                name="inputDescription"
-                                                className="form-control"
-                                                defaultValue={""}
-                                            />
+                                            <input type="text"name="inputDescription" className="form-control"defaultValue={""} />
                                         </div>
                                         <button type="submit" className="btn btn-primary">
                                             Submit
@@ -346,143 +286,60 @@ class ShowProduct extends Component {
                     </div>
                 </div>
 
-                {/* edit product */}
+                {/* Edit product */}
                 <div>
-                    <div
-                        className="modal fade"
-                        id="modelEditProduct"
-                        tabIndex={-1}
-                        role="dialog"
-                        aria-labelledby="modelTitleId"
-                        aria-hidden="true"
-                    >
+                    <div className="modal fade" id="modelEditProduct"tabIndex={-1} role="dialog" aria-labelledby="modelTitleId"aria-hidden="true">
                         <div className="modal-dialog" role="document">
                             <div className="modal-content">
                                 <div className="modal-header">
                                     <h5 className="modal-title">Modal Edit Product</h5>
-                                    <button
-                                        type="button"
-                                        className="close"
-                                        data-dismiss="modal"
-                                        aria-label="Close"
-                                        id="closeModalEditBtn"
-                                    >
+                                    <button type="button"className="close"data-dismiss="modal"aria-label="Close" id="closeModalEditBtn">
                                         <span aria-hidden="true">×</span>
                                     </button>
                                 </div>
                                 <div className="modal-body">
-                                    <form
-                                        onSubmit={this.submitEditProduct}
-                                        encType="multipart/form-data"
-                                    >
+                                    <form onSubmit={this.submitEditProduct}encType="multipart/form-data" >
                                         <div className="form-group">
                                             <label htmlFor="editID">ID</label>
-                                            <input
-                                                type="number"
-                                                className="form-control"
-                                                name="editID"
-                                                id="editID"
-                                                readOnly
-                                            />
+                                            <input type="number" className="form-control" name="editID"  id="editID" readOnly />
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="editName">Name</label>
-                                            <input
-                                                type="text"
-                                                className="form-control"
-                                                name="editName"
-                                                id="editName"
-                                                placeholder="Enter name"
-                                                required
-                                            />
+                                            <input type="text" className="form-control" name="editName"  id="editName" placeholder="Enter name" required />
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="editPrice">Price</label>
-                                            <input
-                                                type="number"
-                                                min={10000}
-                                                className="form-control"
-                                                name="editPrice"
-                                                id="editPrice"
-                                                placeholder="Enter price"
-                                                required
-                                            />
+                                            <input type="number"  min={10000} className="form-control"name="editPrice" id="editPrice"  placeholder="Enter price"  required />
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="editPromotionPrice">
                                                 Promotion Price
                                             </label>
-                                            <input
-                                                type="number"
-                                                min={10000}
-                                                className="form-control"
-                                                name="editPromotionPrice"
-                                                id="editPromotionPrice"
-                                                placeholder="Enter promotion price"
-                                            />
+                                            <input type="number" min={10000}className="form-control" name="editPromotionPrice"  id="editPromotionPrice"  placeholder="Enter promotion price" />
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="editUnit">Unit</label>
-                                            <input
-                                                type="text"
-                                                className="form-control"
-                                                name="editUnit"
-                                                id="editUnit"
-                                                placeholder="Enter unit"
-                                                required
-                                            />
+                                            <input type="text"className="form-control" name="editUnit"id="editUnit" placeholder="Enter unit" required  />
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="editNew">New</label>
-                                            <input
-                                                type="number"
-                                                min={0}
-                                                className="form-control"
-                                                name="editNew"
-                                                id="editNew"
-                                                placeholder="Enter new"
-                                                required
-                                            />
+                                            <input type="number" min={0} className="form-control" name="editNew"id="editNew"  placeholder="Enter new" required/>
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="editType">Type</label>
-                                            <input
-                                                type="number"
-                                                min={1}
-                                                className="form-control"
-                                                name="editType"
-                                                id="editType"
-                                                placeholder="Enter type"
-                                                required
-                                            />
+                                            <input type="number"min={1}className="form-control" name="editType" id="editType" placeholder="Enter type" required />
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="editImage">Image file</label>
-                                            <input
-                                                type="file"
-                                                className="form-control-file"
-                                                name="editImage"
-                                                id="editImage"
-                                                onChange={(e) => this.handleChange(e.target.files)}
-                                            />
+                                            <input type="file"  className="form-control-file"name="editImage"id="editImage"  onChange={(e) => this.handleChange(e.target.files)}/>
                                         </div>
                                         <div className="form-group">
-                                            <img
-                                                id="preview-image-before-edit"
-                                                src="https://www.riobeauty.co.uk/images/product_image_not_found.gif"
-                                                alt="xem trước"
-                                                style={{ maxHeight: 250 }}
-                                            />
+                                            <img id="preview-image-before-edit"  src="https://www.riobeauty.co.uk/images/product_image_not_found.gif"  alt="xem trước" style={{ maxHeight: 250 }} />
                                             {this.previewEditImage()}
                                         </div>
                                         <div className="form-group">
                                             <label htmlFor="editDescription">Description</label>
-                                            <input
-                                                type="text"
-                                                name="editDescription"
-                                                id="editDescription"
-                                                className="form-control"
-                                            />
+                                            <input type="text"name="editDescription" id="editDescription"className="form-control"/>
                                         </div>
                                         <button type="submit" className="btn btn-primary">
                                             Submit
@@ -496,13 +353,7 @@ class ShowProduct extends Component {
 
                 {/* show product */}
                 <div className="container">
-                    <button
-                        type="button"
-                        data-toggle="modal"
-                        data-target="#modelAddProduct"
-                        className="btn btn-primary"
-                        style={{ width: 80 }}
-                    >
+                    <button type="button" data-toggle="modal" data-target="#modelAddProduct"className="btn btn-primary"style={{ width: 80 }} >
                         Add
                     </button>
                     <DataTable title="Show Products" columns={this.columns} data={this.state.products} paginationPerPage={5} defaultSortField="id" pagination />
